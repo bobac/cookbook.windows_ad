17a064c113a9d4ec919221f9f004d8cd49ded2f0
derekgroh <derekgroh@gmail.com>
2018-07-30 22:12:43 -0500

    forest inspec test

---
+++ test/integration/forest/forest_test.rb
@@ -0,0 +1,10 @@
+# # encoding: utf-8
+
+# Inspec test for recipe windows_ad::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe command('[ADSI]::Exists("LDAP://contoso.com")') do
+  its('stdout') { should include 'True' }
+end
