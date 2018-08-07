a72fb43443d057f8bb3cc3006b67512c1eb6b81f
derekgroh <derekgroh@gmail.com>
2018-08-07 04:49:27 -0500

    use contoso.local

--- test/integration/forest/forest_test.rb
+++ test/integration/forest/forest_test.rb
@@ -5,6 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-describe command('[ADSI]::Exists("LDAP://contoso.com")') do
+describe command('[ADSI]::Exists("LDAP://contoso.local")') do
   its('stdout') { should include 'True' }
 end
